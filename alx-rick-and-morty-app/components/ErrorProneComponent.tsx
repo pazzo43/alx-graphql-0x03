57e8d42e5fe074063a163c19aba0b9e2dd5645b0
Patrick NDICUNGUYE <pazzondicunguye43@gmail.com>
2025-12-18 12:05:50 +0200

    Update ErrorProneComponent.tsx

--- alx-rick-and-morty-app/components/ErrorProneComponent.tsx
+++ alx-rick-and-morty-app/components/ErrorProneComponent.tsx
@@ -1,1 +1,35 @@
+# components/ErrorProneComponent.tsx
+import React from 'react';
 
+const ErrorProneComponent: React.FC = () => {
+  // This will force the React rendering phase to fail
+  throw new Error('This is a test error!');
+};
+
+export default ErrorProneComponent;
+
+import ErrorBoundary from '@/components/ErrorBoundary';
+import ErrorProneComponent from '@/components/ErrorProneComponent';
+
+const Home: React.FC = () => {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Welcome to the Rick and Morty App</h1>
+      <p>The content below is wrapped in an Error Boundary:</p>
+      
+      <ErrorBoundary>
+        <ErrorProneComponent />
+      </ErrorBoundary>
+    </div>
+  );
+};
+
+export default Home;
+
+npm run dev
+
+ready - started server on http://localhost:3000
+
+Open:
+
+http://localhost:3000
